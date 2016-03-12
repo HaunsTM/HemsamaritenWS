def758b6463c9ed368e3bda1367feb6b8fa9fde9
Hans Baldetorp <hans.baldetorp@gmail.com>
2016-03-12 10:21:13 +0100

    Har lagt till [ForeignKey] där det behövdes

--- Tellstick/Model/TellstickAction.cs
+++ Tellstick/Model/TellstickAction.cs
@@ -3,6 +3,9 @@
     using Tellstick.Model.Interfaces;
 
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Newtonsoft.Json;
 
     public class TellstickAction : IEntity, ITellstickAction
     {
@@ -16,11 +19,23 @@
 
         #region Navigation properties
 
-
+        [JsonIgnore]
         public virtual PerformedAction PerformedAction { get; set; }
+        [JsonIgnore]
         public virtual TellstickUnit TellstickUnit { get; set; }
+        [JsonIgnore]
         public virtual TellstickActionType TellstickActionType { get; set; }
+        [JsonIgnore]
         public virtual TellstickScheduler TellstickScheduler { get; set; }
+
+        [ForeignKey("PerformedAction")]
+        public int PerformedAction_Id { get; set; }
+        [ForeignKey("TellstickUnit")]
+        public int TellstickUnit_Id { get; set; }
+        [ForeignKey("TellstickActionType")]
+        public int TellstickActionType_Id { get; set; }
+        [ForeignKey("TellstickScheduler")]
+        public int TellstickScheduler_Id { get; set; }
 
         #endregion
 
--- Tellstick/Model/TellstickUnit.cs
+++ Tellstick/Model/TellstickUnit.cs
@@ -4,6 +4,8 @@
 
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     using Newtonsoft.Json;
 
     public class TellstickUnit : IEntity, ITellstickUnit
@@ -24,9 +26,19 @@
         [JsonIgnore]
         public virtual List<TellstickAction> TellstickActions { get; set; }
 
+        [JsonIgnore]
         public virtual TellstickParameter TellstickParameter { get; set; }
+        [JsonIgnore]
         public virtual TellstickProtocol TellstickProtocol { get; set; }
+        [JsonIgnore]
         public virtual TellstickModel TellstickModel { get; set; }
+
+        [ForeignKey("TellstickParameter")]
+        public int TellstickParameter_Id { get; set; }
+        [ForeignKey("TellstickProtocol")]
+        public int TellstickProtocol_Id { get; set; }
+        [ForeignKey("TellstickModel")]
+        public int TellstickModel_Id { get; set; }
 
         #endregion
 
