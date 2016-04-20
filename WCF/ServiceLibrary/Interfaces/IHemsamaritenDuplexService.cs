f026f6986b4f5d82324cd6634b9d2dcd57f28e4a
Hans Baldetorp <hans.baldetorp@gmail.com>
2016-04-20 23:38:07 +0200

    Lagt till en metod i WCF-service som genererar och seedar HemsamaritenDB

--- WCF/ServiceLibrary/Interfaces/IHemsamaritenDuplexService.cs
+++ WCF/ServiceLibrary/Interfaces/IHemsamaritenDuplexService.cs
@@ -1,9 +1,12 @@
 ﻿namespace WCF.ServiceLibrary.Interfaces
 {
+    using System.Security.Cryptography.X509Certificates;
     using System.ServiceModel;
 
     [ServiceContract(CallbackContract = typeof(IHemsamaritenDuplexCallback))]
     public interface IHemsamaritenDuplexService : ISurveillanceCam2DBDuplexService, ITellstickDuplexService
     {
+        [OperationContract(IsOneWay = true)]
+        void CreateAndInitializeHemsamaritenDB();
     }
 }
