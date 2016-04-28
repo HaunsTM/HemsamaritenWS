3f4fb9553a978c9c943bfa2e1e3c2a7d8e830f89
Hans Baldetorp <hans.baldetorp@gmail.com>
2016-04-28 10:22:03 +0200

    Uppdaterat applikationen med en exekverbar windowsservice samt uppdaterat app. config(ar)

---
+++ HemsamaritenWindowsService/WindowsService.cs
@@ -0,0 +1,34 @@
+﻿using System.ServiceModel;
+using System.ServiceProcess;
+
+namespace HemsamaritenWindowsService
+{
+    public partial class WindowsService : ServiceBase
+    {
+
+        internal static ServiceHost myServiceHost = null;
+
+        public WindowsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+            }
+            myServiceHost = new ServiceHost(typeof(WCF.ServiceLibrary.HemsamaritenDuplexService));
+            myServiceHost.Open();
+        }
+        protected override void OnStop()
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+                myServiceHost = null;
+            }
+        }
+    }
+}
