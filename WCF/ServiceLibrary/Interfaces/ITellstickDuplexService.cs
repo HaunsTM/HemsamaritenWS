1d6d7dfa46fa4cdd20d5c51d0998572c4eb1ec74
HaunsTM <hans.baldetorp@gmail.com>
2017-01-01 23:28:23 +0100

    GET

--- WCF/ServiceLibrary/Interfaces/ITellstickDuplexService.cs
+++ WCF/ServiceLibrary/Interfaces/ITellstickDuplexService.cs
@@ -55,14 +55,14 @@ namespace WCF.ServiceLibrary.Interfaces
         void RemoveTellstickDevice(int nativeDeviceId);
 
         [OperationContract(IsOneWay = true)]
-        [WebInvoke(Method = "POST",
+        [WebInvoke(Method = "GET",
                     BodyStyle = WebMessageBodyStyle.WrappedRequest,
                     RequestFormat = WebMessageFormat.Json,
                     ResponseFormat = WebMessageFormat.Json)]
         void TurnOnTellstickDevice(int nativeDeviceId);
 
         [OperationContract(IsOneWay = true)]
-        [WebInvoke(Method = "POST",
+        [WebInvoke(Method = "GET",
                     BodyStyle = WebMessageBodyStyle.WrappedRequest,
                     RequestFormat = WebMessageFormat.Json,
                     ResponseFormat = WebMessageFormat.Json)]
