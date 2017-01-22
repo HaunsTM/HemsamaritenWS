7a982b4089989a857150339c9e4162bacce8bf05
HaunsTM <hans.baldetorp@gmail.com>
2017-01-22 11:13:58 +0100

    Changed         List<Tellstick.Model.ViewModel.UnitPerformedAction> LatestRegisteredAction(string unitIdListDelimitedString); to become "POST" (from "GET")

--- WCF/ServiceLibrary/Interfaces/ITellstickDuplexService.cs
+++ WCF/ServiceLibrary/Interfaces/ITellstickDuplexService.cs
@@ -85,7 +85,7 @@ namespace WCF.ServiceLibrary.Interfaces
 
         #endregion
 
-        [WebInvoke(Method = "GET",
+        [WebInvoke(Method = "POST",
                    BodyStyle = WebMessageBodyStyle.WrappedRequest,
                    RequestFormat = WebMessageFormat.Json,
                    ResponseFormat = WebMessageFormat.Json)]
