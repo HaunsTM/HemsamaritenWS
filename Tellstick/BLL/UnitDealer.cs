a53a7b81e7b87d0fc9011ba89075c09c881987db
Hemsamariten <hemsamariten@gmail.com>
2017-07-30 19:30:06 +0200

    New functions and CORS

---
+++ Tellstick/BLL/UnitDealer.cs
@@ -0,0 +1,45 @@
+﻿namespace Tellstick.BLL
+{
+    using System.Linq;
+    using log4net;
+    using Tellstick.BLL.Interfaces;
+    using Tellstick.Model;
+
+    public class UnitDealer : IUnitDealer
+    {
+
+        private static readonly ILog log =
+            LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public string DbConnectionStringName { get; private set; }
+
+        public UnitDealer(string dbConnectionStringName)
+        {
+            DbConnectionStringName = dbConnectionStringName;
+        }
+
+        public Unit UnitBy(int id)
+        {
+            using (var db = new Tellstick.Model.TellstickDBContext(DbConnectionStringName))
+            {
+                var unit = from u in db.Units
+                            where u.Id == id
+                            select u;
+
+                return unit.FirstOrDefault();
+            }
+        }
+
+        public Unit UnitByNativeDevice(int nativeDeviceId)
+        {
+            using (var db = new Tellstick.Model.TellstickDBContext(DbConnectionStringName))
+            {
+                var unit = (from u in db.Units
+                            where u.NativeDeviceId == nativeDeviceId
+                            select u).FirstOrDefault();
+
+                return unit;
+            }
+        }
+    }
+}
