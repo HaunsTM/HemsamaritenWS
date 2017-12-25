5175d8c18457c17fb61dc6920fc243581a0a5482
HaunsTM <hans.baldetrp@gmail.com>
2017-12-25 21:31:48 +0100

    Updated deploy scripts

--- HemsamaritenWindowsService/deployScript.ps1
+++ HemsamaritenWindowsService/deployScript.ps1
@@ -1,6 +1,59 @@
-$remoteComputer = "HEMSAMARITEN"
+param ([string]$solutionDir, [string]$projectDir, [string]$hemsamaritenIP, [string]$remoteShare, [string]$remoteWindowsServiceName)
 
-$canConnectToRemoteComputer = Test-Connection -$remoteComputer
 
-#Copy the files to the remote server
-#net use "\\[COMPUTER]\c$" "[PASSWORD]" /USER:"[USERNAME]" /persistent:no
+
+Write-Host "Trying to establish connection $remoteShare";
+$canConnectToRemoteShare = [System.IO.Directory]::Exists($remoteShare);
+if ($canConnectToRemoteShare) {
+ 
+[Console]::Beep(600, 800)
+    Write-Host "Connection can be established to $remoteShare";
+    
+[Console]::Beep(600, 800)
+    $currentService = Get-Service -Name $remoteWindowsService -ComputerName $hemsamaritenIP
+
+    #uninstall service on remote computer
+    if ($currentService) {
+
+        Write-host "Stopping Service $currentService from $hemsamaritenIP" -ForegroundColor Green
+        sc.exe \\$hemsamaritenIP stop $currentService #Stop Service
+        Start-Sleep -s 10 #Pause 10 seconds to wait for service stopped
+
+        Write-host "Disabling Service $currentService from $hemsamaritenIP" -ForegroundColor Green
+        sc.exe \\$hemsamaritenIP config $currentService start= disabled #Disable Service
+
+        Write-host "Kill MMC Service from $hemsamaritenIP" -ForegroundColor Green
+        taskkill /s $hemsamaritenIP /IM mmc /f #Kill mmc if exist which will prevent to delete service
+
+        Write-host "Removing Service $currentService from $hemsamaritenIP" -ForegroundColor Green
+        sc.exe \\$hemsamaritenIP delete $currentService #Delete Service
+
+        Write-host "Querying Service $currentService from $hemsamaritenIP" -ForegroundColor Green
+        sc.exe \\$hemsamaritenIP qc $currentService #Query Service agai
+
+    } else {
+    
+        $remoteWindowsService + " is not installed on computer answering at " + $hemsamaritenIP + "."
+    }
+
+    #copy 
+
+    Write-Host "Begin publishing $remoteWindowsService"
+
+    robocopy /is "$(SolutionDir)TestProjectHtml" %destination% /mir
+
+
+     if ($lastexitcode -eq 0)
+     {
+          write-host "Robocopy succeeded"
+     }
+    else
+    {
+          write-host "Robocopy failed with exit code:" $lastexitcode
+    }
+
+
+} else {
+    
+    Write-Host "Couldn't establish connection to $remoteShare";
+}
