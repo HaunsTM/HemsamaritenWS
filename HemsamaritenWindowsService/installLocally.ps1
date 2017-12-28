7b2fb6f44bea9b32c307eabf8b0721c025369dbf
HaunsTM <hans.baldetrp@gmail.com>
2017-12-28 13:29:56 +0100

    Install locally

--- HemsamaritenWindowsService/installLocally.ps1
+++ HemsamaritenWindowsService/installLocally.ps1
@@ -6,13 +6,8 @@ $targetDir = $targetDir -replace '\\\s$', '';
 $targetFileName = $targetFileName -replace '\\\s$', '';
 
 
-Write-Host "*** ...$targetFileName...";
-
 Write-Host "*** Trying to establish connection to $remoteShare";
-$canConnectToRemoteShare = [System.IO.Directory]::Exists($remoteShare);
- 
-if ($canConnectToRemoteShare) {
- 
+
     Write-Host "*** Trying to get service $remoteWindowsServiceName on $hemsamaritenIP";
     $currentService = Get-Service -Name $remoteWindowsServiceName -ComputerName $hemsamaritenIP    
 
@@ -26,9 +21,6 @@ if ($canConnectToRemoteShare) {
         Write-host "*** Removing Service $currentService from $hemsamaritenIP" -ForegroundColor Green
         sc.exe \\$hemsamaritenIP delete $currentService #Delete Service
 
-        #Write-host "*** Querying Service $currentService from $hemsamaritenIP" -ForegroundColor Green
-        #sc.exe \\$hemsamaritenIP qc $currentService #Query Service agai
-
     } else {
     
         $remoteWindowsServiceName + " was not installed on computer answering at " + $hemsamaritenIP + "."
@@ -36,27 +28,6 @@ if ($canConnectToRemoteShare) {
 
     #copy     
     Write-Host "*** Begin publishing $remoteWindowsServiceName"
-    Write-Host "*** Copying files..."
     robocopy "$targetDir" "$remoteShare\bin\Deploy" /is /mir
-
-    if ($lastexitcode -eq 0 -or $lastexitcode -eq 1)
-    {
-        write-host "*** Robocopy succeeded"
-        write-host "*** Installing "
-        
-        Write-host "*** $remoteShare\bin\Deploy\$targetFileName" -ForegroundColor Green
-		
-		Invoke-Command -ComputerName "HEMSAMARITEN" -Credential $hemsamaritenAdministratorCreds -ScriptBlock {sc.exe create newservice $remoteWindowsServiceName binpath= "$remoteShare\bin\Deploy\$targetFileName" start= auto}
-        
-        $deployedService = Get-Service -Name $remoteWindowsServiceName -ComputerName $hemsamaritenIP
-    }
-    else
-    {
-          write-host "Robocopy failed with exit code:" $lastexitcode
-    }
-
-
-} else {
-    
-    Write-Host "Couldn't establish connection to $remoteShare";
-}
+	
+	sc.exe create newservice $remoteWindowsServiceName binpath= "$remoteShare\bin\Deploy\$targetFileName" start= auto
