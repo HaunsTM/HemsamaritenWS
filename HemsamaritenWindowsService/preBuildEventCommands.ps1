44d92d1e955b500c98a06a1ef19d24cb01ccded1
HaunsTM <hans.baldetorp@gmail.com>
2017-12-30 21:31:24 +0100

    Added check to verify Admin rights when using build "Run on Hemsamariten locally"

---
+++ HemsamaritenWindowsService/preBuildEventCommands.ps1
@@ -0,0 +1,16 @@
+﻿param ([string]$configurationName)
+
+ $isRunningAsAdministrator = [bool]([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator);
+
+if ($isRunningAsAdministrator) {
+	#has administrator rights
+
+} else {
+	if ($configurationName = "Run locally on Hemsamariten") {
+		
+        $wshell = New-Object -ComObject Wscript.Shell
+        $wshell.Popup("You have to run Visual Studio as Administrator when building with '$configurationName'.",0,"Error",0x0 + 0x30)
+		
+		exit 1
+	}
+}
